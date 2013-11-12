36641e38a3516f6074fc8f960cc8b5e373788a31
6929423 <6929423@qq.com>
2013-11-12 15:00:57 +0800

    添加查询api

--- server/routes.js
+++ server/routes.js
@@ -17,10 +17,26 @@ route['/test'] = function (req, res) {
 }
 
 /**
-* /heros
+* /hero列表
 * */
-route['/heros'] = function (req, res) {
-	res.write(JSON.stringify(req.params));
-	res.write("hello");
-	res.end();
+route['/hero/list'] = function (req, res) {
+	db('db.heros').find(120,function(r) {
+		var data = r.documents;
+		res.writeHeader(200, {'Content-Type':'text/javascript;charset=UTF-8'});
+		res.write(JSON.stringify(data));
+		res.end();
+	});
+}
+
+/**
+* /hero攻略
+* 根据hero_title,hero_chName,hero_enName,hero_id查询
+* */
+route['/hero'] = function (req, res) {
+	db('db.gonglves').find(req.params,function(r) {
+		var data = r.documents;
+		res.writeHeader(200, {'Content-Type':'text/javascript;charset=UTF-8'});
+		res.write(JSON.stringify(data));
+		res.end();
+	});
 }
